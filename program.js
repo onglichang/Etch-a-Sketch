727c46bf7928b64ae145e239f6765df2b0db0947
onglichang <onglichang@hotmail.com>
2021-12-03 14:26:27 +0800

    Add margins to 16x16 grid

--- program.js
+++ program.js
@@ -4,10 +4,11 @@ const container = document.querySelector('.container');
 for (let i=0; i < 16; i++) {
     // Add in horizontal div into container
     let horDiv = document.createElement('div');
-    horDiv.setAttribute('style', 'display:flex');
+    horDiv.setAttribute('style', 'display:flex; flex: 1');
     for (let j=0; j < 16; j++) {
         //Inside each horizontal div, add 16 vertical divs
         let verDiv = document.createElement('div');
+        verDiv.setAttribute('style', 'padding-bottom: 20%; border: 2px solid black; flex: 1');
         horDiv.appendChild(verDiv);
     }
     container.appendChild(horDiv);
