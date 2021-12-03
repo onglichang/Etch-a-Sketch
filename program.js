1ebdb0ade4b2dc343619f2b15e0057626c911b17
onglichang <onglichang@hotmail.com>
2021-12-03 15:20:51 +0800

    Correct 16x16 grid to be properly squared and aligned

--- program.js
+++ program.js
@@ -4,11 +4,12 @@ const container = document.querySelector('.container');
 for (let i=0; i < 16; i++) {
     // Add in horizontal div into container
     let horDiv = document.createElement('div');
-    horDiv.setAttribute('style', 'display:flex; flex: 1');
+    horDiv.setAttribute('style', 'display: flex; flex: 1;');
+
     for (let j=0; j < 16; j++) {
         //Inside each horizontal div, add 16 vertical divs
         let verDiv = document.createElement('div');
-        verDiv.setAttribute('style', 'padding-bottom: 20%; border: 2px solid black; flex: 1');
+        verDiv.setAttribute('style', 'flex: 1');
         horDiv.appendChild(verDiv);
     }
     container.appendChild(horDiv);
