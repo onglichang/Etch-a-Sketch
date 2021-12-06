d91c7b5743535bb7c6d7f7862364d169aee43513
onglichang <onglichang@hotmail.com>
2021-12-06 14:39:21 +0800

    Add hover effect for grid colour change

--- program.js
+++ program.js
@@ -10,7 +10,14 @@ for (let i=0; i < 16; i++) {
         //Inside each horizontal div, add 16 vertical divs
         let verDiv = document.createElement('div');
         verDiv.setAttribute('style', 'flex: 1');
+        //Add event listener to each inner-box div
+        verDiv.addEventListener('mouseenter', changeColor)
         horDiv.appendChild(verDiv);
     }
     container.appendChild(horDiv);
+}
+
+// Set up hover effect so that grid divs change color when mouse passes over them
+function changeColor(e) {
+    e.target.setAttribute('class', 'black-box');
 }
